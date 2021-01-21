d42133488eaac9d57e42f96d46d3611ccd455e8f
wangyunbo <1810962886@qq.com>
2021-01-21 23:56:58 +0800

    scafford

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,30 @@ void main() {
 }
 
 class MyApp extends  StatelessWidget {
+
+  void answerQuestion() {
+    print('Answer chosen');
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('hello!'),);
+    var questions = [
+      'whats your favorite color?',
+      'whats your favorite animal',
+    ];
+    return MaterialApp(home: Scaffold(
+      appBar: AppBar(title: Text('My first app'),),
+      body: Column(
+        children: [
+          Text('the question'),
+          RaisedButton(child: Text('Answer 1'),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion,),
+          RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion,),
+        ],
+      ),
+    ),
+    );
   }
 }
